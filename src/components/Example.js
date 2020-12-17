bc4aeb910652ec44df2a87db063d83973fb4a144
yujin779 <51077021+yujin779@users.noreply.github.com>
2020-12-17 23:39:20 +0900

    α

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
-import { TouchableOpacity, Text, View, ScrollView } from "react-native";
-import { randomValueFromList, nonCurrentList } from "../common/listutil";
+import React from "react";
+import { Text, View } from "react-native";
+import { randomValueFromList } from "../common/listutil";
 import { list } from "./data";
 import styles from "../styles";
 import { createGlobalState } from "react-hooks-global-state";
-import { ExampleCurrentTitle, ExampleCurrentView } from "./ExampleCurrent";
+import {
+  ExampleCurrentTitle,
+  ExampleCurrentView,
+  CurrentDeleteButton,
+  NextButton
+} from "./ExampleCurrent";
 
 const initialState = {
   examplesList: list,
@@ -25,8 +30,6 @@ const ExampleListCount = () => {
 };
 
 const Example = () => {
-  const [examplesList, setExamplesList] = useGlobalState("examplesList");
-  const [exampleText, setExampleText] = useGlobalState("exampleText");
   return (
     <View>
       <ExampleListCount />
@@ -34,28 +37,8 @@ const Example = () => {
       <ExampleCurrentView />
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.currentDeleteButton}
-          onPress={() => {
-            const newList = nonCurrentList(examplesList)(exampleText);
-            setExamplesList(newList);
-            setExampleText(randomValueFromList(newList));
-          }}
-        >
-          <Text style={styles.buttonText}>
-            この関数を表示させない{"\n"}リストから削除
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.nextButton}
-          onPress={() => {
-            setExampleText(
-              randomValueFromList(nonCurrentList(examplesList)(exampleText))
-            );
-          }}
-        >
-          <Text style={styles.buttonText}>次を表示</Text>
-        </TouchableOpacity>
+        <CurrentDeleteButton />
+        <NextButton />
       </View>
     </View>
   );
